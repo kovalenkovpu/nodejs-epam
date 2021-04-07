a505a0c2f3ecfdcb228ddd6a2e5fe7817b7e353f
Pavel <pavel_kovalenkov@epam.com>
2021-04-07 18:06:40 +0300

    TASK3-PSQL-and-layered-architecture
    * attempt 3 to turn on SSL

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,9 +3,12 @@ import process from 'process';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
   dialect: 'postgres',
+  protocol: 'postgres',
   dialectOptions: {
-    ssl: true,
-    rejectUnauthorized: false,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
 
